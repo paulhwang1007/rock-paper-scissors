2b76b465f135099d48ff1f5535664c057654eb35
Paul Hwang <hwpaul1007@gmail.com>
2024-07-19 21:36:39 -0700

    Enhance readability of the code.
    Added some indents and made any fixes to the readability of the code. Tried thinking through the getHumanChoice() if the user doesn't put any of the valid options, but couldn't.

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-console.log("hello world");
-// The Problem: We want to be able to play rock paper scissors
-// it will be played entirely in the console so doesn't involve much HTML
+    // The Problem: We want to be able to play rock paper scissors
+    // it will be played entirely in the console so doesn't involve much HTML
 
-// Plan: 
-// There is no user interace, it all happens in the console
-// We will add a graphical user interface (GUI) later
-// Inputs: rock, paper, scissors
-// Outputs: rock, paper, scissors; win, lose, tie
-// Given the inputs and outputs, we want to check which combinations
-// of rock, paper, and scissors result in win, lose, or tie
+    // Plan: 
+    // There is no user interface, it all happens in the console
+    // We will add a graphical user interface (GUI) later
+    // Inputs: rock, paper, scissors
+    // Outputs: rock, paper, scissors; win, lose, tie
+    // Given the inputs and outputs, we want to check which combinations
+    // of rock, paper, and scissors result in win, lose, or tie
 
-// Pseudocode:
-// When a user inputs an option of either rock, paper, or scissors
-// Return a random output of either rock, paper, or scissors
-// Given the input and output, check what combinations results in a win, loss, or tie
-// Return the result
-// If the result is a tie, play again until the result is either a win or loss
+    // Pseudocode:
+    // When a user inputs an option of either rock, paper, or scissors
+    // Return a random output of either rock, paper, or scissors
+    // Given the input and output, check what combinations results in a win, loss, or tie
+    // Return the result
+    // If the result is a tie, play again until the result is either a win or loss
 
-// Divide and Conquer
-
-// Solving Fizz Buzz
+    // Divide and Conquer
 
 // Project: Rock Paper Scissors
 
@@ -50,56 +47,55 @@ function getHumanChoice() {
 // Logic to play a game (5 rounds)
 function playGame() {
 
-// Keep track of the score
-let humanScore = 0;
-let computerScore = 0;
+    // Keep track of the score
+    let humanScore = 0;
+    let computerScore = 0;
 
-// Logic to play a single roud
-function playRound(humanChoice, computerChoice) {
+    // Logic to play a single roud
+    function playRound(humanChoice, computerChoice) {
 
-    if (computerChoice.toLowerCase() === "rock") {
-        if (humanChoice.toLowerCase() === "paper") {
-            ++humanScore;
-            console.log("You win! Paper beats Rock!");
-        } else if (humanChoice.toLowerCase() === "scissors") {
-            ++computerScore;
-            console.log("You lose! Rock beats Scissors!");
-        } else {
-            console.log("Tie! Play Again!");
+        if (computerChoice.toLowerCase() === "rock") {
+            if (humanChoice.toLowerCase() === "paper") {
+                ++humanScore;
+                console.log("You win! Paper beats Rock!");
+            } else if (humanChoice.toLowerCase() === "scissors") {
+                ++computerScore;
+                console.log("You lose! Rock beats Scissors!");
+            } else {
+                console.log("Tie! Play Again!");
+            }
         }
+
+        if (computerChoice.toLowerCase() === "paper") {
+            if (humanChoice.toLowerCase() === "scissors") {
+                ++humanScore;
+                console.log("You win! Scissors beats Paper!");
+            } else if (humanChoice.toLowerCase() === "rock") {
+                ++computerScore;
+                console.log("You lose! Paper beats Rock!");
+            } else {
+                console.log("Tie! Play Again!");
+            }
+        }
+
+        if (computerChoice.toLowerCase() === "scissors") {
+            if (humanChoice.toLowerCase() === "rock") {
+                ++humanScore;
+                console.log("You win! Rock beats Scissors!");
+            } else if (humanChoice.toLowerCase() === "paper") {
+                ++computerScore;
+                console.log("You lose! Scissors beats Paper!");
+            } else {
+                console.log("Tie! Play Again!");
+            }
+        }
+        return console.log(`Human Score: ${humanScore}; Computer Score: ${computerScore}`);
     }
 
-    if (computerChoice.toLowerCase() === "paper") {
-        if (humanChoice.toLowerCase() === "scissors") {
-            ++humanScore;
-            console.log("You win! Scissors beats Paper!");
-        } else if (humanChoice.toLowerCase() === "rock") {
-            ++computerScore;
-            console.log("You lose! Paper beats Rock!");
-        } else {
-            console.log("Tie! Play Again!");
-        }
+    // Loop playRound() 5 times
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
     }
-
-    if (computerChoice.toLowerCase() === "scissors") {
-        if (humanChoice.toLowerCase() === "rock") {
-            ++humanScore;
-            console.log("You win! Rock beats Scissors!");
-        } else if (humanChoice.toLowerCase() === "paper") {
-            ++computerScore;
-            console.log("You lose! Scissors beats Paper!");
-        } else {
-            console.log("Tie! Play Again!");
-        }
-    }
-    return console.log(`Human Score: ${humanScore}; Computer Score: ${computerScore}`);
-}
-
-for (let i = 0; i < 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
 
 }
 
